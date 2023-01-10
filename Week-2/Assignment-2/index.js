fa311549c143151ae1d76fcca83fceee7feb08be
Jason Tsai <see89826@gmail.com>
2023-01-11 00:50:19 +0800

    add avg function in assignment-2

---
+++ Week-2/Assignment-2/index.js
@@ -0,0 +1,27 @@
+function avg(data) {
+  // your code here
+  let total = 0
+  const array = data.products
+  for (const i of array) {
+    total += i.price
+  }
+  let average = total / data.size
+  console.log(average)
+}
+
+avg({
+  size: 3,
+  products: [{
+      name: 'Product 1',
+      price: 100,
+    },
+    {
+      name: 'Product 2',
+      price: 700,
+    },
+    {
+      name: 'Product 3',
+      price: 250,
+    },
+  ],
+})
