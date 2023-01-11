6f277d709958343e1175c861a16912d1eb7b795f
Jason Tsai <see89826@gmail.com>
2023-01-11 18:26:41 +0800

    add binarySearchPosition function im index.js

---
+++ Week-2/Assignment-5/index.js
@@ -0,0 +1,31 @@
+function binarySearchPosition(numbers, target) {
+  // your code here
+  let startPoint = 0
+  let endPoint = numbers.length - 1
+  let midPoint = 0
+  do {
+    midPoint = Math.floor((startPoint + endPoint) / 2)
+    if (startPoint > endPoint) {
+      return -1
+      break
+    }
+    if (numbers[midPoint] === target) {
+      return midPoint
+    } else if (numbers[midPoint] > target) {
+      endPoint = midPoint - 1
+    } else {
+      startPoint = midPoint + 1
+    }
+  } while (numbers[midPoint] !== target)
+}
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 2));
+// should print 1
+
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 6));
+// should print 3
+
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 1));
+// should print 0
+
+console.log(binarySearchPosition([1, 2, 5, 6, 7, 8, 9, 11, 12], 10));
+// should print -1 
