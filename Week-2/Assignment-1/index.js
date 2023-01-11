ab2597cbc2ba6ff1168317bb1ebe514564fbd737
Jason082666 <105638495+Jason082666@users.noreply.github.com>
2023-01-11 12:07:38 +0800

    Update index.js

--- Week-2/Assignment-1/index.js
+++ Week-2/Assignment-1/index.js
@@ -8,10 +8,11 @@ function max(numbers) {
   return comparison
 }
 
+// test function max
 console.log(max([0, -1, -2, -3, 9]) === 9)
 console.log(max([0, -1, -1, -1, -1]) === 0)
 console.log(max([0, 6, 6, 6, 6]) === 6)
-
+// should print true
 
 function findPosition(numbers, target) {
   // your code here, for-loop method preferred
@@ -24,7 +25,10 @@ function findPosition(numbers, target) {
   }
   return position
 }
+
+// test function findPosition
 console.log(findPosition([1, 2, 2, 2, 2, 4, 5], 2) === 1)
 console.log(findPosition([1, 5, 3, 4, 5, 4, 3], 5) === 1)
 console.log(findPosition([1, 2, 3, 4, 5, 6, 5], 7) === -1)
 console.log(findPosition([1, 2, 3, 4, 5, 6, 5], '2') === 1)
+// should print true
