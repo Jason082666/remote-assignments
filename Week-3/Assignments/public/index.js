07821f61c6bc8e626971bee15f130d2d491e5421
Jason082666 <105638495+Jason082666@users.noreply.github.com>
2023-01-19 21:11:19 +0800

    Update index.js

--- Week-3/Assignments/public/index.js
+++ Week-3/Assignments/public/index.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('button')
 const input = document.querySelector('input')
 button.addEventListener('click',()=>{
-  // button.preventDefault()
   const value = document.querySelector('input').value
   input.value = value
 })
