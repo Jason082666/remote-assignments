438b4e7a97ba8cec6982585f4e9290ceb191f819
Jason Tsai <see89826@gmail.com>
2023-01-28 23:11:04 +0800

    set up

---
+++ Week-4/Assignment-1/index.js
@@ -0,0 +1,35 @@
+function delayedResult(n1, n2, delayTime, callback) {
+// your code here
+const result = n1 +n2
+callback = (result)=>{console.log(result)}
+setTimeout(callback,delayTime,result)
+}
+
+
+delayedResult(4, 5, 3000, function (result) {
+console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
+delayedResult(-5, 10, 2000, function (result) {
+console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
+
+
+function delayedResultPromise(n1, n2, delayTime) {
+  // your code here
+  return new Promise((resolve, reject) => {
+    const result = n1 + n2
+    setTimeout(() => {
+      resolve(result)
+    }, delayTime)
+  })
+}
+delayedResultPromise(4, 5, 3000).then(console.log);
+// 9 (4+5) will be shown in the console after 3 seconds
+
+async function main() {
+  // your code here, you should call delayedResultPromise here and get the result using async/await.
+  const result = await delayedResultPromise(4, 5, 3000)
+  console.log(result)
+
+}
+main(); // result will be shown in the console after <delayTime> seconds
---
+++ Week-4/Assignment-2/index.js
@@ -0,0 +1,44 @@
+function ajax(src,callback) {
+  // your code here
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', src)
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText)
+        resolve(callback(data))
+      } else {
+        reject(Error(xhr.statusText))
+      }
+    }
+    xhr.onerror = () => {
+      reject(Error('A network error occured !'))
+    }
+    xhr.send()
+  })
+}
+
+function render(data) {
+  // your code here
+  data.map((element)=>{
+    const name = element.name
+    const price = element.price
+    const description = element.description
+    const box = document.createElement('div')
+    box.className = 'box'
+    box.innerHTML = `<div class='name'>${name}</div> 
+    <div class='price'>${price}</div>
+    <div class='description'>${description}</div>`
+    const container = document.querySelector('.container')
+    container.appendChild(box)
+  })
+  // document.createElement() and appendChild() methods are preferred.
+}
+
+
+ajax(
+  'https://appworks-school.github.io/Remote-Assignment-Data/products',
+  function (response) {
+    render(response);
+  }
+); // you should get product information in JSON format and render data in the page
